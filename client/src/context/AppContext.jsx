c65c1a2f9d915841a4c140ebbe69d903e57f1f02
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-06-14 01:51:31 +0300

    backend cors update

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -15,6 +15,8 @@ const AppContextProvider = (props) => {
 
 	const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000"
 
+	console.log("-------->"+backendUrl)
+
 	const navigate = useNavigate();
 
 	useEffect(() => {
--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ const app = express();
 
 await connectDB();
 
-app.use(cors());
+app.use(cors({
+	origin: ['http://localhost:5173', 'https://image-generator-t526.vercel.app'],
+	credentials: true,
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	allowedHeaders: ['Content-Type', 'Authorization', 'token']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
