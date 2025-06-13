30a629e5861e2d68e5d75a338e7019614abdd8b7
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-06-14 01:25:51 +0300

    backend cors update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-app.use(cors)
+app.use(cors())
 
 // const allowedOrigins = ['http://localhost:5173', 'https://your-frontend.vercel.app'];
 
