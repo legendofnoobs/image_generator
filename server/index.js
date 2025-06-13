34800c6576677e4b7a104ea6b3acc516e8fab29e
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-06-14 01:59:25 +0300

    added frontend link to the allowed origins

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 await connectDB();
 
 app.use(cors({
-	origin: ['http://localhost:5173', 'https://image-generator-t526.vercel.app'],
+	origin: ['http://localhost:5173', 'https://image-generator-liard-theta.vercel.app'],
 	credentials: true,
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization', 'token']
