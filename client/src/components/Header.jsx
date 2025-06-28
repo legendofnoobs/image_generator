4ae1053fd76569875e9050c9522229df7b3688ab
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-06-28 19:47:52 +0300

    refactor(Header): update button layout for improved responsiveness

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -21,27 +21,23 @@ const Header = () => {
 
 			<h1 className="text-4xl max-w-[300px] sm:text-7xl sm:max-w-[890px] mx-auto mt-20 text-center text-white">Use Your Imagination to Create and Edit Faces</h1>
 
-			<div className="flex flex-col">
-				<div className="flex gap-4 justify-center flex-wrap">
-					<button className="sm:text-lg text-white bg-purple-700 w-auto mt-4 px-12 py-2.5 flex items-center justify-center gap-2 rounded-full relative z-10 hover:bg-purple-900 transition-colors" onClick={() => navigate("/docs")}>
-						Documentation
-					</button>
-					<button className="sm:text-lg text-white bg-purple-700 w-auto mt-4 px-12 py-2.5 flex items-center justify-center gap-2 rounded-full relative z-10 hover:bg-purple-900 transition-colors" onClick={onClickHandler}>
-						Generate Face
-					</button>
-				</div>
-				<div>
-					{user &&
-						<div className="flex gap-4 flex-wrap justify-center">
-							<button className="sm:text-lg text-white bg-purple-700 w-auto mt-4 px-12 py-2.5 flex items-center justify-center gap-2 rounded-full relative z-10 hover:bg-purple-900 transition-colors" onClick={() => navigate("/edit")}>
-								Upload, Edit images
-							</button>
-							<button className="sm:text-lg text-white bg-purple-700 w-auto mt-4 px-12 py-2.5 flex items-center justify-center gap-2 rounded-full relative z-10 hover:bg-purple-900 transition-colors" onClick={() => navigate("/saved")}>
-								Show Saved Images
-							</button>
-						</div>
-					}
-				</div>
+			<div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center mt-8">
+				<button className="sm:text-lg text-white bg-purple-700 w-full px-12 py-2.5 flex items-center justify-center gap-2 rounded-full relative z-10 hover:bg-purple-900 transition-colors" onClick={() => navigate("/docs")}>
+					Documentation
+				</button>
+				<button className="sm:text-lg text-white bg-purple-700 w-full px-12 py-2.5 flex items-center justify-center gap-2 rounded-full relative z-10 hover:bg-purple-900 transition-colors" onClick={onClickHandler}>
+					Generate Face
+				</button>
+				{user && (
+					<>
+						<button className="sm:text-lg text-white bg-purple-700 w-full px-12 py-2.5 flex items-center justify-center gap-2 rounded-full relative z-10 hover:bg-purple-900 transition-colors" onClick={() => navigate("/edit")}>
+							Upload, Edit Images
+						</button>
+						<button className="sm:text-lg text-white bg-purple-700 w-full px-12 py-2.5 flex items-center justify-center gap-2 rounded-full relative z-10 hover:bg-purple-900 transition-colors" onClick={() => navigate("/saved")}>
+							Show Saved Images
+						</button>
+					</>
+				)}
 			</div>
 		</div>
 	)
